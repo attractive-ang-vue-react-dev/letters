56011361ab3f67afaf4afcba8ce77c87f0104607
Sonicscream <Sonicscream@users.noreply.github.com>
2019-12-22 13:21:26 -0500

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -143,12 +143,6 @@
                 <div class="col-md-12">
                 </div>
             </div>
-            <div class="col-md-12">
-                <a class="btn btn-sm btn-primary"style="margin-left:6%; margin-right:6%;width: 87%;background-color: white; color:#2c2c2c;"href="/">
-                <img src="google.svg" style="width:8%;">
-                Sign Up with Google</a>
-                <p style="margin-left:6%; margin-right:6%;font-size:14px; color:white;text-align:center;">By creating an account, you agree to the Terms of Service, and Privacy Policy.</p>
-            </div>
         </div>
     </div>
 </div>
